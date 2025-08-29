dae150d3487524634cddd58f49d671ee4adaa11c
Keroshi <bgathendu@live.com>
2025-08-29 13:46:20 +0300

    Modified agent and prompts.py files

--- agent.py
+++ agent.py
@@ -1,6 +1,8 @@
+import asyncio
 from dotenv import load_dotenv
 from livekit import agents
-from livekit.agents import AgentSession, Agent, RoomInputOptions
+from livekit.agents import AgentSession, Agent, RoomInputOptions, UserInputTranscribedEvent, ConversationItemAddedEvent, \
+    AgentFalseInterruptionEvent
 from livekit.plugins import (
     cartesia,
     google,
@@ -10,7 +12,9 @@ from livekit.plugins import (
 )
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 from prompts import AGENT_INSTRUCTION, SESSION_INSTRUCTION
+from livekit.agents.llm import ImageContent, AudioContent, ChatMessage
 from tools import get_weather, search_web
+import logging
 
 load_dotenv(".env.local")
 
@@ -20,33 +24,56 @@ class Assistant(Agent):
         super().__init__(
             instructions=AGENT_INSTRUCTION,
             # stt=deepgram.STT(model="nova-3", language="multi"),
-            llm=google.beta.realtime.RealtimeModel(voice="Aoede", temperature=0.0),
+            llm=google.beta.realtime.RealtimeModel(voice="Aoede", temperature=0.0, ),
             tools=[get_weather, search_web],
             # tts=cartesia.TTS(model="sonic-2", voice="f786b574-daa5-4673-aa0c-cbe3e8534c02"),
-            # vad=silero.VAD.load(),
+            vad=silero.VAD.load(),
             # turn_detection=MultilingualModel(),
         )
 
 
 async def entrypoint(ctx: agents.JobContext):
-    session = AgentSession(
+    session = AgentSession()
 
-    )
+    @session.on("user_input_transcribed")
+    def on_user_input_transcribed(event: UserInputTranscribedEvent):
+        transcript = event.transcript.strip()
+        logging.info(
+            f"USER({event.speaker_id}): {transcript} "
+            f"(lang={event.language}, final={event.is_final})"
+        )
+        print(f"USER({event.speaker_id}): {transcript}")
+
+    @session.on("conversation_item_added")
+    def on_conversation_item_added(event: ConversationItemAddedEvent):
+        print(
+            f"Conversation item added from {event.item.role}: {event.item.text_content}. interrupted: {event.item.interrupted}"
+        )
+        for content in event.item.content:
+            if isinstance(content, str):
+                print(f" - text: {content}")
+            elif isinstance(content, ImageContent):
+                print(f" - image: {content.image}")
+            elif isinstance(content, AudioContent):
+                print(f" - audio: {content.frame}, transcript: {content.transcript}")
 
     await session.start(
         room=ctx.room,
         agent=Assistant(),
         room_input_options=RoomInputOptions(
-            # For telephony applications, use `BVCTelephony` instead for best results
             video_enabled=True,
             noise_cancellation=noise_cancellation.BVC(),
         ),
     )
 
+    # (Optional) startup greeting
     await session.generate_reply(
-        instructions=SESSION_INSTRUCTION
+        instructions=SESSION_INSTRUCTION,
+        allow_interruptions=False,
     )
 
+
+logging.basicConfig(filename="./logs/app.log", level=logging.INFO)
 
 if __name__ == "__main__":
     agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
